4295ccc08f3f744e514ebae88a9c60ed7f1fbb7d
zzqluvmyj <1639147710@qq.com>
2019-04-27 22:42:37 +0800

    code auto generate alt+insert

---
+++ u5/src/Test.java
@@ -0,0 +1,20 @@
+//自动代码生成,alt+insert
+public class Test{
+    private int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Test() {
+    }
+
+    @Override
+    public String toString() {
+        return "Test{}";
+    }
+}
